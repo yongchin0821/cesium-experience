a35fc05b72622a13da8198e98676a19f36bc11bb
りりこ <yongchin39@qq.com>
2025-05-31 10:32:29 +0800

    feat: init the project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import * as Cesium from "cesium";
+import "./Widgets/widgets.css";
+
+window.CESIUM_BASE_URL = "/";
+
+// const cesiumContainer = ref(null);
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer("cesiumContainer");
+});
+</script>
+
+<template>
+  <div id="cesiumContainer" ref="cesiumContainer"></div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
